dd8d95725196b9356e7215514afd4e9bc2898ac9
Abraham Cocoletzi Z <20192271@garzas.uatx.mx>
2022-07-25 21:09:06 -0500

    Consultas

---
+++ front-end/consulta.php
@@ -0,0 +1,20 @@
+<?php
+
+//Conexion a la base de datos
+$pdo = new PDO(
+    'mysql:host=localhost;dbname=eppw',
+    'root',
+    '',
+);
+
+//Preparando el query con pdo
+$query = "select * from productos1";
+$stmt = $pdo->prepare($query);
+$stmt->execute();
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode((object)array(
+    "succes" => true,
+    "data" => $data
+));
+
+?>
